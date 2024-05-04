99c4be8c8ece2c24897c004ce7e9787ab0593b18
Artem Naboka <83280960+SkyFoxNa@users.noreply.github.com>
2024-05-05 02:09:44 +0300

    fix tag
    Добавил валидацию при вводе тега (мин 3 мах 25)
    
    Добавил проверку на уникальность тега
    
    Убрал User, он там не надо

--- src/routes/tags.py
+++ src/routes/tags.py
@@ -1,10 +1,10 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException, Depends, status
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, HTTPException, Depends, status, Query
 
 from src.database.db import get_db
-from src.entity.models import User, Role
+from src.entity.models import Role
+from sqlalchemy.ext.asyncio import AsyncSession
 from src.schemas.tag import TagModel, TagResponse
 from src.repository import tags as repository_tags
 from src.conf.messages import AuthMessages
@@ -18,14 +18,13 @@ access_to_route_all = RoleAccess([Role.admin, Role.moderator])
 
 
 @router.get("/", response_model=List[TagResponse], dependencies=[Depends(access_to_route_all)])
-async def read_tags(skip: int = 0, limit: int = 100, db: Session = Depends(get_db),
-                    _: User = Depends(auth_service.get_current_user)):
+async def read_tags(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
     tags = await repository_tags.get_tags(skip, limit, db)
     return tags
 
 
 @router.get("/{tag_id}", response_model=TagResponse, dependencies=[Depends(access_to_route_all)])
-async def read_tag(tag_id: int, db: Session = Depends(get_db), _: User = Depends(auth_service.get_current_user)):
+async def read_tag(tag_id: int, db: AsyncSession = Depends(get_db)):
     tag = await repository_tags.get_tag(tag_id, db)
     if tag is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Tag not found")
@@ -33,16 +32,16 @@ async def read_tag(tag_id: int, db: Session = Depends(get_db), _: User = Depends
 
 
 @router.post("/", response_model=TagResponse, dependencies=[Depends(access_to_route_all)])
-async def create_tag(body: TagModel, db: Session = Depends(get_db), _: User = Depends(auth_service.get_current_user)):
-    tag = await repository_tags.create_tag(body, db)
+async def create_tag(name_tag: str = Query(..., min_length=3, max_length=25),
+                     db: AsyncSession = Depends(get_db)):
+    tag = await repository_tags.create_tag(name_tag, db)
     if tag is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=AuthMessages.verification_error)
     return tag
 
 
 @router.put("/{tag_id}", response_model=TagResponse, dependencies=[Depends(access_to_route_all)])
-async def update_tag(body: TagModel, tag_id: int, db: Session = Depends(get_db),
-                     _: User = Depends(auth_service.get_current_user)):
+async def update_tag(body: TagModel, tag_id: int, db: AsyncSession = Depends(get_db)):
     tag = await repository_tags.update_tag(tag_id, body, db)
     if tag is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -51,7 +50,7 @@ async def update_tag(body: TagModel, tag_id: int, db: Session = Depends(get_db),
 
 
 @router.delete("/{tag_id}", response_model=TagResponse, dependencies=[Depends(access_to_route_all)])
-async def remove_tag(tag_id: int, db: Session = Depends(get_db), _: User = Depends(auth_service.get_current_user)):
+async def remove_tag(tag_id: int, db: AsyncSession = Depends(get_db)):
     tag = await repository_tags.remove_tag(tag_id, db)
     if tag is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
